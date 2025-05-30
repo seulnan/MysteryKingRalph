1e7853c204ab5b02e53888b94bae94c9a3ad8e4f
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-05-30 18:05:46 +0900

    chore: StartScreen을 StartScene으로 이름 통일성있게 변경

--- src/flow/StorySteps.tsx
+++ src/flow/StorySteps.tsx
@@ -4,7 +4,7 @@ import StoryScene from "../components/scenes/StoryScene";
 import SuspectOne from "../components/scenes/SuspectOne";
 import SuspectTwo from "../components/scenes/SuspectTwo";
 import SuspectThree from "../components/scenes/SuspectThree";
-import StartScreen from "../components/scenes/StartScreen";
+import StartScene from "../components/scenes/StartScene";
 import TextScene from "../components/scenes/TextScene";
 import AlibiOne from "../components/scenes/AlibiOne";
 export interface StoryStep {
@@ -13,13 +13,14 @@ export interface StoryStep {
 }
 
 export const StorySteps: StoryStep[] = [
-  // { id: "intro", element: <IntroScene /> },
-  // { id: "story", element: <StoryScene /> },
-  // { id: "suspect-1", element: <SuspectOne /> },
-  // { id: "suspect-2", element: <SuspectTwo /> },
-  // { id: "suspect-3", element: <SuspectThree /> },
-  // { id: "investigation-1", element: <TextScene title="첫번째 용의자" subtitle="백설공주 조사" /> },
-  // { id: "alibi-1", element: <AlibiOne /> },
+  { id: "start", element: <StartScene /> },
+  { id: "intro", element: <IntroScene /> },
+  { id: "story", element: <StoryScene /> },
+  { id: "suspect-1", element: <SuspectOne /> },
+  { id: "suspect-2", element: <SuspectTwo /> },
+  { id: "suspect-3", element: <SuspectThree /> },
+  { id: "investigation-1", element: <TextScene title="첫번째 용의자" subtitle="백설공주 조사" /> },
+  { id: "alibi-1", element: <AlibiOne /> },
   { id: "puzzle-1", element: <DwarfsPuzzle /> },
   { id: "investigation-2", element: <TextScene title="두번째 용의자" subtitle="라푼젤 조사" /> },
   { id: "ending", element: <TextScene title="..는 다음에 공개" /> },
