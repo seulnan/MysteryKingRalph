e9fc66195bd4bd5f3cacd5f20af7e190cf3eab5e
wwwlnyy <wwwlnyy@gmail.com>
2025-05-30 15:06:53 +0900

    Feat: StroyScene, SuspectOne 슬라이드 구현

--- src/components/scenes/StoryScene.tsx
+++ src/components/scenes/StoryScene.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { ReactTyped } from 'react-typed';
+import { useFlow } from "../../contexts/FlowProvider.tsx";
+
+
+const StoryTexts = [
+  "마녀가 자신의 성에서 쓰러진 채 발견됐다\n" +
+  "조사 결과, 사인은 청산가리에 의한 독살로 밝혀졌으며\n" +
+  "입가에서 청산가리 흔적이 검출되었다.\n" +
+  "범행 시점은 이틀 전 오후 7시에서 9시 사이로 추정된다.\n\n" +
+  "왕실 수사국은 수사 끝에,\n" +
+  "마녀와 깊은 원한 관계에 있던 세 명의 주요 용의자를 특정했다.\n",
+];
+
 export default function StroyScene() {
+  const { goNext } = useFlow();
+  const [isTypingDone, setIsTypingDone] = useState(false);
+
+  const handleClick = () => {
+    if (isTypingDone)
+      goNext();
+  }
+
   return (
-    <div>
-      <h1>스토리씬</h1>
+    <div 
+      className="relative flex w-[120rem] h-[67.5rem] bg-[#080A09] mx-auto overflow-hidden"
+      onClick={handleClick}>
+      <img
+        src="/images/witch.png"
+        alt="죽은 마녀"
+        className="w-[119rem] h-[45.938rem] absolute top-[10.813rem] left-[26.75rem] mix-blend-luminosity"
+      />
+      <ReactTyped
+        strings={StoryTexts}
+        typeSpeed={50}
+        backSpeed={30}
+        loop={false}
+        className="font-hanseokbong-des-400 text-[2.25rem] leading-[160%] absolute top-[22rem] left-[7.375rem] text-white tracking-[-0.0225rem] 
+        w-[105.25rem] h-[30.5rem]"
+        style={{ whiteSpace: 'pre-line' }}
+        onComplete={() => setIsTypingDone(true)}
+      />
     </div>
-  );
+  )
 }
--- src/components/scenes/SuspectOne.tsx
+++ src/components/scenes/SuspectOne.tsx
@@ -1,7 +1,39 @@
+import { ReactTyped } from "react-typed";
+
+const Texts = [
+  "과거 마녀가 건넨 독사과를 먹고 죽을 뻔한 사건을 겪었다.\n\n" + 
+  "이후 심각한 트라우마로 인해 과일을 입에 대지 못하게 되었고, 결국 비타민 결핍으로 괴혈병에 걸렸다.\n" +
+  "그녀 곁엔 언제나 충직한 일곱 난쟁이들이 함께하고 있다."
+];
+
 export default function SuspectOne() {
   return (
-    <div>
-      <h1>첫 퍼즐</h1>
+    <div className="relative flex w-[120rem] h-[67.5rem] bg-[#080A09] mx-auto overflow-hidden">
+      <img 
+      src="/images/snowwhite_backgrond.png" 
+      alt="백설공주 배경"
+      className="relative flex w-[120rem] h-[67.5rem] mx-auto"
+      />
+      <img
+        src="/images/snowwhite.png"
+        alt="백설공주"
+        className="absolute top-[5.3rem] left-[0] w-[64rem] h-[62.25rem]"
+      />
+      <div className="absolute top-[8.56rem] left-[52.9rem] w-[60.125rem] h-[50.365rem] rounded-[1.25rem] bg-[rgba(0,0,0,0.6)]
+]">
+        <p className="font-hanseokbong-subtitle-700 absolute top-[8rem] left-[4rem] 
+          text-white tezt-[3rem] tracking-[-0.075rem]">
+          {`첫번째 용의자 : 백설 공주`}
+        </p>
+        <ReactTyped
+          strings={Texts}
+          typeSpeed={50}
+          backSpeed={30}
+          loop={false}
+          className="font-hanseokbong-des-300 absolute top-[18rem] left-[4rem] 
+          text-white text-[2.25rem] tracking-[-0.0375rem] leading-[160%] w-[51.25rem] [Gapyeong Hanseokbong]"
+          />
+      </div>
     </div>
   );
 }
